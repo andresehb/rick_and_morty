af253fe50c91c6bfe9feb9a0118e8c1affe9f831
Andres Hernandez <hernandezboscanandre@gmail.com>
2023-04-05 17:08:28 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Cards from './components/Cards.jsx';
+import Nav from './components/Nav';
+import axios from 'axios';
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Detail from './components/Detail';
+import About from './components/About';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [characters, setCharacters] = useState([]);
+
+   function onSearch(id) {
+      axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
+         if (data.name) {
+            let noRepeat = characters.find((character) => character.id === data.id);
+            if (noRepeat) {
+               alert('Character is already being displayed')
+            } else {
+               setCharacters((oldChars) => [...oldChars, data]);
+            }
+         } else {
+            window.alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+
+   function onClose(id){
+      setCharacters((oldChars) => {
+         return oldChars.filter((character) => character.id !== id)
+      })
+   }
+
+   return (
+      <div className='App'>
+         <Nav onSearch={onSearch} />
+         <Routes>
+            <Route path='/' element={<Login/>}></Route>
+            <Route path='/home' element={<Cards onClose={onClose} characters={characters}/>}></Route>
+            <Route path='/about' element={<About/>}></Route>
+            <Route path='/detail/:id' element={<Detail/>}></Route>
+         </Routes>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import './Card.css'
+
+export default function Card({id, name, status, species, gender, origin, image, onClose}) {
+   return (
+      <div className="card">
+         <button onClick={() => onClose(id)}>X</button>
+         <Link to={`/detail/${id}`}>
+            <h2>{name}</h2>
+            <h2>{status}</h2>
+            <h2>{species}</h2>
+            <h2>{gender}</h2>
+            <h2>{origin}</h2>
+            <img src={image} alt={name}/>
+         </Link>
+      </div>
+   );
+}
